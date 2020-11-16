423e3739de9c00d0014eeb61ed9c5df0fb12ad57
sidahmed <ahorob57@gmail.com>
2020-11-16 16:10:42 +0100

    add hi method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        User user = new User();
+        User user = new User("Sodium");
+        // System.out.println(user.name);
+        user.sayHi();
     }
 }
--- src/com/company/User.java
+++ src/com/company/User.java
@@ -3,6 +3,11 @@ package com.company;
 public class User {
     // fields (attributes)
     public String name;
-
+    public User(String name) {
+        this.name = name;
+    }
     // methods
+    public void sayHi() {
+        System.out.println("Hi there, my name is " + this.name);
+    }
 }
