ba8699c7bfb0f9c1dbf608f699111d41129b556a
sidahmed <ahorob57@gmail.com>
2020-11-17 12:56:42 +0100

    ToolType

--- src/com/company/state/Canvas.java
+++ src/com/company/state/Canvas.java
@@ -1,11 +1,25 @@
 package com.company.state;
 
 public class Canvas {
-    public void mouseDown() {
+    private ToolType currentTool;
 
+    public void mouseDown() {
+        if (currentTool == ToolType.SELECTION) {
+            System.out.println("Selection Icon");
+        } else if (currentTool == ToolType.BRUSH) {
+            System.out.println("Brush Icon");
+        } else if (currentTool == ToolType.ERASER) {
+            System.out.println("Eraser Icon");
+        }
     }
 
     public void mouseUp() {
-
+        if (currentTool == ToolType.SELECTION) {
+            System.out.println("draw dash");
+        } else if (currentTool == ToolType.BRUSH) {
+            System.out.println("draw line");
+        } else if (currentTool == ToolType.ERASER) {
+            System.out.println("erase something");
+        }
     }
 }
